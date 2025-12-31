b0eefca00b5b4458cd122c94af29098f854a466b
Yashapalsinhchauhan <yashubhachauhan4@gmail.com>
2025-12-31 08:59:47 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,63 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x=10
+print(x)
+10
+a=5
+print(-a)
+-5
+p=10
+q=5
+print(p>q)
+True
+print (p==q)
+False
+x=true
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a|b)
+7
+>>> numbers=[1,2,3,4,5,6]
+>>> print(2 in number)
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    print(2 in number)
+NameError: name 'number' is not defined. Did you mean: 'numbers'?
+>>> number=[1,2,3,4,5,6]
+>>> print(2 in number)
+True
+>>> print(5 not in number)
+False
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> a=5
+>>> b=9
+>>> print(a+b)
+14
+>>> print(a-b)
+-4
+>>> print(a*b)
+45
+>>> print(a/b)
+0.5555555555555556
