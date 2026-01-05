99a7438a8fe3cdcea7f277e5b53afd531f93571b
Yashapalsinhchauhan <yashubhachauhan4@gmail.com>
2026-01-05 10:27:30 +0530

    Add files via upload

---
+++ data tyape.py
@@ -0,0 +1,30 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello")
+hello
+>>> x=20
+>>> print(x)
+20
+>>> x=20.5
+>>> print(x)
+20.5
+>>> x=1j
+>>> print(x)
+1j
+>>> x=["apple","banana","cherry"]
+>>> print(x)
+['apple', 'banana', 'cherry']
+>>> x=[{"apple","banana","cherry"}]
+>>> print(x)
+[{'banana', 'cherry', 'apple'}]
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+>>> x=True
+>>> print(x)
+True
+>>> x=[{"name":"john","age":"36"}]
+>>> print(x)
+[{'name': 'john', 'age': '36'}]
